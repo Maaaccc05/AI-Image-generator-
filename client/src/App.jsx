88a6161f99f9812399511271915056fe726f3ca6
Mayuresh Kamble <mackam30@gmail.com>
2025-08-12 22:55:39 +0530

    Add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,12 @@ const App = () => {
 
         <Link to = "/create" className="font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
       </header>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/create post' element={<Create />}></Route>
+        </Routes>
+      </main>
    </BrowserRouter>
   )
 }
