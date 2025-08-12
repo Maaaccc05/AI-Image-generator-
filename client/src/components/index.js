96ae1afe01f71a76b1420ee812636c8864e583df
Mayuresh Kamble <mackam30@gmail.com>
2025-08-12 23:18:14 +0530

    some more changes

---
+++ client/src/components/index.js
@@ -0,0 +1,9 @@
+import Card from './Card'
+import Formfiled from './FormFiled'
+import Loader from './Loader'
+
+export{
+    Card,
+    Formfiled,
+    Loader
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
+import { Card, Formfiled, Loader } from '../components'
 
 const Home = () => {
   return (
