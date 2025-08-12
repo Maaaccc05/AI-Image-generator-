421573df31a5ac0293ef875b0711c1d8ee598773
Mayuresh Kamble <mackam30@gmail.com>
2025-08-12 23:35:26 +0530

    Done for day

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,8 +3,15 @@ import React, {useState, useEffect} from 'react'
 import { Card, Formfiled, Loader } from '../components'
 
 const Home = () => {
+
+  const [loading, setLoading] = useState(false)
+  const[allPosts, setAllpost] = useState(null)
   return (
-    <div>Home</div>
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>The Community</h1>
+      </div>
+    </section>
   )
 }
 
