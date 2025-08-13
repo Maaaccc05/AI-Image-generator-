baa4aa57b96ef533a33d9775441ed30856ddb8d6
Mayuresh Kamble <mackam30@gmail.com>
2025-08-13 11:18:43 +0530

    some changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,10 +7,15 @@ const Home = () => {
   const [loading, setLoading] = useState(false)
   const[allPosts, setAllpost] = useState(null)
   return (
-    <section className='max-w-7xl mx-auto'>
+    <section className='max-w-7xl '>
       <div>
         <h1 className='font-extrabold text-[#222328] text-[32px]'>The Community</h1>
+      <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'>Browse through imaginative creation by DALL-E AI</p>
       </div>
+      <div className='mt-16'>
+        <Formfiled />
+      </div>
+
     </section>
   )
 }
