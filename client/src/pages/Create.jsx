cfee24fcb09ea6f70b4dfbb2b8b84a150c146910
Mayuresh Kamble <mackam30@gmail.com>
2025-08-14 23:29:31 +0530

    done for day

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -3,6 +3,19 @@ import { useNavigate } from 'react-router-dom'
 import { preview } from '../assets'
 import { getRandomPrompt } from '../utils'
 import { Formfiled, Loader } from '../components'
+import FormFiled from '../components/FormFiled'
+
+const handleSubmit = () => {
+
+}
+
+const handleChange = (e) => {
+
+}
+
+const handleSurpriseMe = () => {
+
+}
 
 const Create = () => {
 
@@ -20,8 +33,32 @@ const Create = () => {
     <section className='max-w-7xl '>
       <div>
         <h1 className='font-extrabold text-[#222328] text-[32px]'>Create Your thoughts Here</h1>
-        <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'>Browse through imaginative creation by DALL-E AI</p>
+        <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'>Create imaginative Images by DALL-E AI</p>
       </div>
+
+      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-5'>
+          <FormFiled 
+            LabelName = "Your Name"
+            type = "text"
+            name = "name"
+            placholder = "Mac"
+            value = {form.name}
+            handleChange = {handleChange}
+          />
+
+          <FormFiled 
+            LabelName = "prompt"
+            type = "text"
+            name = "prompt"
+            placholder = "a painting of a fox in the style of Starry Night"
+            value = {form.prompt}
+            handleChange = {handleChange}
+            isSurpriseMe  
+            handleSurpriseMe = {handleSurpriseMe}
+          />
+        </div>
+      </form>
     </section>
   )
 }
