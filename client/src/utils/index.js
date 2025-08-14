fc002763e285f416a0a4fa357b20f541cda6e962
Mayuresh Kamble <mackam30@gmail.com>
2025-08-14 13:56:35 +0530

    break for now

---
+++ client/src/utils/index.js
@@ -0,0 +1,10 @@
+import { surpriceMePrompts } from '../constants';
+
+export function getRandomPrompt(prompt) {
+    const randomIndex = Math.floor(Math.random() * surpriceMePrompts.length)
+    const randomPrompt = surpriceMePrompts[randomIndex]
+
+    if(randomPrompt === prompt) return getRandomPrompt(prompt)
+
+    return randomPrompt;
+}
